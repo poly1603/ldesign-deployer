a87217fbc78c893b21c4fac97dd9c9aa624156b7
swimly <979741120@qq.com>
2025-10-22 21:17:08 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# @ldesign/deployer
+部署工具 - 多环境部署、Docker/K8s、CI/CD模板、回滚机制、蓝绿部署
+
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @ldesign/deployer - 部署工具
+ */
+export class Deployer {
+  async deploy(env: string) { console.info(`Deploying to ${env}...`) }
+  async rollback(version: string) { console.info(`Rolling back to ${version}...`) }
+}
+export function createDeployer() { return new Deployer() }
+
